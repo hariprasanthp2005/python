200bc7805d76f66a0d66b1eca04045d65cb006e5
Hari Prasanth <hariprasanthp2005@gmail.com>
2026-06-25 11:07:58 +0530

    add

---
+++ addition/add.py
@@ -0,0 +1,4 @@
+num1=int(input("enter the no1:"))
+num2=int(input("enter the no2:"))
+
+print(num1+num2)
---
+++ addition/error.py
@@ -0,0 +1,12 @@
+#syntax error
+#emp!name="hp" 
+#print(emp!name)
+
+#identation error
+#def main():
+#print("hello")
+#main()
+
+#invalid syntax
+#class for:   #for is a keyword
+ #   pass
