6b8250acdee91d0ab106bd8ff5e80c396942869f
Hari Prasanth <hariprasanthp2005@gmail.com>
2026-06-25 11:10:10 +0530

    vfco

---
+++ vfco/vfco.py
@@ -0,0 +1,21 @@
+#variable
+
+eid=101
+name="hp"
+
+#function
+def main():
+    print("login success")
+main()
+
+#class
+class employee:
+    pass
+
+#dict object
+
+emp={
+    "eid":101,
+    "name":"hp"
+}
+print(emp["name"])
