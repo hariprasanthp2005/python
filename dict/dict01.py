4e7886d372e601a6b6cce440af072b48ee2a1aa6
Hari Prasanth <hariprasanthp2005@gmail.com>
2026-06-29 13:03:44 +0530

    dict

---
+++ dict/dict01.py
@@ -0,0 +1,22 @@
+emp = {
+    "name":"hp",
+    "age": 21,
+    "location":"banglore",
+    "gender":"male"
+}
+#read
+print(emp)
+
+#update
+emp["age"]=22
+print(emp)
+
+#type
+print(type(emp))
+
+#delete
+del (emp["age"])
+print(emp)
+
+
+
