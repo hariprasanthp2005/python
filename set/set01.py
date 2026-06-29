6a8643e6b52c10da5551f06b2bf71bc01bd60dfa
Hari Prasanth <hariprasanthp2005@gmail.com>
2026-06-29 12:30:31 +0530

    set

---
+++ set/set01.py
@@ -0,0 +1,13 @@
+uids={101,102,103,104}
+print(uids[1])  #type error . set object is not applicable
+
+
+
+#update
+
+'''
+set Object is not subscriptable 
+Set - indexing is not possible
+    - Order not maintained
+    set is a mutable 
+'''
---
+++ set/set02.py
@@ -0,0 +1,13 @@
+a1={}
+a2={101}
+
+print(a1)
+print(a2)
+
+print(type(a1))
+print(type(a2))
+
+
+s2=set()
+print(s2)
+print(type(s2))
