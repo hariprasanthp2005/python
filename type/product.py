afcbd06cbb28347d41d6fad4eb7a7f62333135e3
Hari Prasanth <hariprasanthp2005@gmail.com>
2026-06-29 12:01:06 +0530

    type

---
+++ type/product.py
@@ -0,0 +1,17 @@
+#create
+price=500
+rating=3.1
+product_name="suits"
+size=[30,32,36,38,40]
+
+#read
+print(price)
+print(rating)
+print(product_name)
+print(size)
+
+print(type(price))
+print(type(rating))
+print(type(product_name))
+print(type(size))
+
