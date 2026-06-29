30e60765023ee65708920920047fbaa2be209150
Hari Prasanth <hariprasanthp2005@gmail.com>
2026-06-29 12:27:59 +0530

    dict

---
+++ dict/dict.py
@@ -0,0 +1,24 @@
+#create
+d1={}  #empty dict
+emp={
+    "eid":101,
+    "name":"hp",
+    "esal":100000
+}
+#insert
+emp["city"]="banglore"
+print(emp)
+
+#read
+print(d1)
+print(type(d1))
+
+print(type(emp))
+print(emp)
+
+print(emp["name"])
+
+#group of key value pairs as one entity
+#duplicate keys are not allowed but values are allowed
+#indxing is not allowed
+
---
+++ dict/dict1.py
@@ -0,0 +1,7 @@
+user={
+    'email':'hp@gmail.com',  #syntax error
+    'email':'vk@gmail.com',
+    'email':'rs@gmail.com'
+}
+
+print(user)
