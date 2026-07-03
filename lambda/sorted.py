624d5e9e1dbd5f02cf4bf17dcff1a5ab255fcfb6
Hari Prasanth <hariprasanthp2005@gmail.com>
2026-07-03 12:07:11 +0530

    lambda

--- lambda/sorted.py
+++ lambda/sorted.py
@@ -1,5 +1,19 @@
 #sorted() → custom sorting
-marks=[("hp",98),("vk",89),("ab",95),("cg",70)]
+marks=[
+    ("hp",98),
+    ("vk",89),
+    ("ab",95),
+    ("cg",70)
+]
 
 marks1=sorted(marks,key=lambda x:x[1],)
 print(marks1)
+
+jobs = [
+    ("TCS", 30000),
+    ("Infosys", 50000),
+    ("Wipro", 25000)
+]
+
+sorted_jobs = sorted(jobs, key=lambda x: x[1])
+print(sorted_jobs)
